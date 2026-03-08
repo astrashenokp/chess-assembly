3584e9337e5981f3a950f2095a091b2a963d688e
Rinata Abdurakhimova <r.abdurakhimova@ukma.edu.ua>
2026-03-08 12:45:16 +0200

    refactor: change extrn modile system to INCLUDE

--- main.asm
+++ main.asm
@@ -4,17 +4,26 @@
 INCLUDE shared.inc
 
 .DATA
-; main state
-    board  DB 64 DUP(0)      ;playing board 8x8
-    current_turn  DB 0       ; 0-white 1-black
-    white_king_pos  DB ?    
-    black_king_pos  DB ?
-    move_list  DB 256 DUP(0) ;buffer for move
+ 
+; Board representation
+; index = row * 8 + col
+ 
+
+board DB 64 DUP(?)
+
+ 
+; Global state
+current_turn    DB 0      ; 0 = white, 1 = black
+white_king_pos  DB ?
+black_king_pos  DB ?
+
+ 
+; Move buffer
+; format: from_row, from_col, to_row, to_col
+move_list   DB 256 DUP(?)
+move_count  DB 0
     
 .CODE
-; tell that procs exist in other files
-EXTRN init_video_mode:PROC
-EXTRN draw_board:PROC
 
 start:
     mov ax, @data
@@ -26,5 +35,8 @@ start:
 exit_program:
     mov ah, 4Ch
     int 21h
+
+INCLUDE display.asm
+INCLUDE game.asm  
 
 END start

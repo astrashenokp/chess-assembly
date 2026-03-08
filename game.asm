19c89a4616e248e62dfae285906dd5f3b18b54fa
Rinata Abdurakhimova <r.abdurakhimova@ukma.edu.ua>
2026-03-08 12:45:44 +0200

    refactor: remove global state data in game.asm because it is stored in main

--- game.asm
+++ game.asm
@@ -1,25 +1,4 @@
 INCLUDE shared.inc
-
-.DATA
- 
-; Board representation
-; index = row * 8 + col
- 
-
-board DB 64 DUP(?)
-
- 
-; Global state
-current_turn    DB 0      ; 0 = white, 1 = black
-white_king_pos  DB ?
-black_king_pos  DB ?
-
- 
-; Move buffer
-; format: from_row, from_col, to_row, to_col
-move_list   DB 256 DUP(?)
-move_count  DB 0
-
  
 ; Direction tables
 rook_dirs:

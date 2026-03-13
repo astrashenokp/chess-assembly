2fe0400936c9182f2605b9346a8e367a1ece9d38
Polina Astrashenok <astrashenok225@gmail.com>
2026-03-14 01:54:55 +0200

    Test: add parameters to main.asm logic calls to prevent stack corruption

--- src/main.asm
+++ src/main.asm
@@ -19,17 +19,50 @@ start:
     mov ax, @data
     mov ds, ax
 
+    ; Initialize board array with starting position
+    call init_board
+
+    ; Set video mode and render the board
     call init_video_mode
     call draw_board
-    call init_board
+
+    ; Test get_legal_moves 
+    push 6      
+    push 4      
     call get_legal_moves
+
+    ; Test execute_move
+    push 6      
+    push 4      
+    push 4      
+    push 4      
     call execute_move
+
+    ; Test is_in_check 
+    push 0     
     call is_in_check
+
+    ; Test is_square_attacked 
+    push 4      
+    push 4      
+    push 1      
     call is_square_attacked
+
+    ; Test is_checkmate 
+    push 0      
     call is_checkmate
+
+    ; Test ai_turn 
     call ai_turn
 
 exit_program:
+    ; Wait for any keypress to keep the screen open
+    mov ah, 00h
+    int 16h
+
+    mov ax, 0003h
+    int 10h
+
     mov ah, 4Ch
     int 21h
 

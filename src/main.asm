15028950b2efcffdeb73d57084e081cd46a16bbc
Rinata Abdurakhimova <r.abdurakhimova@ukma.edu.ua>
2026-03-13 16:56:45 +0200

    refactor: move all the DATA to game.asm and use EXTRN for all the procedures

--- src/main.asm
+++ src/main.asm
@@ -2,31 +2,18 @@
 .STACK 256
 
 INCLUDE shared.inc
-
-.DATA
- 
-; Board representation
-; index = row * 8 + col
- 
-
-board DB 64 DUP(?)
-
- 
-; Global state
-current_turn    DB 0      ; 0 = white, 1 = black
-white_king_pos  DB ?
-black_king_pos  DB ?
-
- 
-; Move buffer
-; format: from_row, from_col, to_row, to_col
-move_list   DB 256 DUP(?)
-move_count  DB 0
     
 .CODE
 
 EXTRN init_video_mode:PROC
 EXTRN draw_board:PROC
+EXTRN init_board:PROC
+EXTRN execute_move:PROC
+EXTRN get_legal_moves:PROC
+EXTRN is_in_check:PROC
+EXTRN is_square_attacked:PROC
+EXTRN is_checkmate:PROC
+EXTRN ai_turn:PROC
 
 start:
     mov ax, @data
@@ -34,6 +21,13 @@ start:
 
     call init_video_mode
     call draw_board
+    call init_board
+    call get_legal_moves
+    call execute_move
+    call is_in_check
+    call is_square_attacked
+    call is_checkmate
+    call ai_turn
 
 exit_program:
     mov ah, 4Ch

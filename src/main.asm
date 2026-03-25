f4373c5404ceda0cfe45e0e6e46e274423372367
Polina Astrashenok <astrashenok225@gmail.com>
2026-03-25 14:55:10 +0200

    feat: add mouse support

--- src/main.asm
+++ src/main.asm
@@ -11,7 +11,9 @@ INCLUDE shared.inc
     is_selected DB 0    ; 0 = no piece selected, 1 = piece selected
     from_row    DW 0
     from_col    DW 0
-    promotion_msg DB 'Choose: 1-Q 2-R 3-B 4-N',0
+    promotion_msg DB 'Promote: 1-Q 2-R 3-B 4-N',0
+
+    need_redraw DB 1
 
     EXTRN move_list:BYTE
     EXTRN move_count:WORD
@@ -47,7 +49,18 @@ start:
     call init_board
     call init_video_mode
 
+    mov ax, 0000h       
+    int 33h
+    mov ax, 0001h       
+    int 33h
+
 game_loop:
+    cmp need_redraw, 1
+    jne check_input     
+
+    mov ax, 0002h
+    int 33h
+
     call draw_board
     call draw_status      
     
@@ -62,8 +75,22 @@ skip_highlights:
     mov cl, al
     call draw_cursor
 
+    mov ax, 0001h
+    int 33h
+
+    mov need_redraw, 0
+
+check_input:
+    mov ah, 01h         
+    int 16h
+    jnz has_key         ; Трамплін для клавіатури
+    jmp check_mouse
+
+has_key:
     mov ah, 00h
     int 16h
+    
+    mov need_redraw, 1  
 
     cmp ah, 48h     
     jne not_up
@@ -98,6 +125,51 @@ esc_cancel_selection:
 
 ignore_key:
     jmp game_loop   
+
+
+check_mouse:
+    mov ax, 0003h       
+    int 33h
+
+    test bx, 1          
+    jnz wait_mouse_release  ; Трамплін для мишки
+    jmp game_loop
+
+wait_mouse_release:
+    mov ax, 0003h
+    int 33h
+    test bx, 1
+    jnz wait_mouse_release
+
+    shr cx, 1
+    shr cx, 1
+    shr cx, 1           
+
+    shr dx, 1
+    shr dx, 1
+    shr dx, 1           
+
+    sub dx, 2
+    jl ignore_mouse
+    shr dx, 1
+    cmp dx, 7
+    jg ignore_mouse
+
+    sub cx, 5
+    jl ignore_mouse
+    shr cx, 1
+    shr cx, 1
+    cmp cx, 7
+    jg ignore_mouse
+
+    mov cursor_row, dx
+    mov cursor_col, cx
+    mov need_redraw, 1  
+    jmp select_cell     
+
+ignore_mouse:
+    jmp game_loop
+
 
 move_up:
     cmp cursor_row, 0
@@ -176,6 +248,7 @@ do_move:
     cmp waiting_for_promotion, 0
     je move_done
 
+    mov need_redraw, 1
     call draw_board
     mov ax, cursor_row
     mov ch, al
@@ -185,6 +258,8 @@ do_move:
 
     call handle_promotion
     call clear_promotion_prompt
+
+    mov need_redraw, 1
     call draw_board
     
     mov ax, cursor_row
@@ -217,7 +292,10 @@ cancel_selection:
     mov is_selected, 0
     jmp game_loop
 
+
 handle_promotion PROC
+    mov ax, 0002h
+    int 33h
     call draw_promotion_prompt
 
 promotion_key_loop:
@@ -241,21 +319,29 @@ promotion_key_loop:
 choose_queen:
     push PROMOTE_QUEEN
     call finalize_promotion
+    mov ax, 0001h
+    int 33h
     ret
 
 choose_rook:
     push PROMOTE_ROOK
     call finalize_promotion
+    mov ax, 0001h
+    int 33h
     ret
 
 choose_bishop:
     push PROMOTE_BISHOP
     call finalize_promotion
+    mov ax, 0001h
+    int 33h
     ret
 
 choose_knight:
     push PROMOTE_KNIGHT
     call finalize_promotion
+    mov ax, 0001h
+    int 33h
     ret
 handle_promotion ENDP
 
